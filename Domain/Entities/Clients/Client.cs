f78e0070977d71abeedcc465643bff30790fc891
Feitoza <thiagobritofeitoza@gmail.com>
2023-04-04 13:51:31 -0300

    feat: implemented factory static method to create client

--- Domain/Entities/Clients/Client.cs
+++ Domain/Entities/Clients/Client.cs
@@ -7,6 +7,30 @@ namespace PicPayLite.Domain.Clients
         public string Email { get; private set; } = string.Empty;
         public ClientType Type { get; private set; }
         public Document Document { get; private set; }
+
+        private Client(
+            Guid id, 
+            string name, 
+            string email, 
+            ClientType type, 
+            Document document)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Type = type;
+            Document = document;
+        }
+
+        public static Client Create(
+            Guid id,
+            string name,
+            string email,
+            ClientType type,
+            Document document)
+        {
+            return new Client(id, name, email, type, document);
+        }
     }
 }
 
