82334d30cf0be341f36172972b9f0a148982d000
Feitoza <thiagobritofeitoza@gmail.com>
2023-04-04 13:49:20 -0300

    feat: added request pattern for create client

---
+++ Presentation/RequestsPattern/CreateClientRequest.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using PicPayLite.Domain.Clients;
+
+namespace PicPayLite.Presentation.RequestsPattern
+{
+    public record CreateClientRequest
+    {
+        [Required]
+        public string Name { get; init; }
+        [Required]
+        public string Email { get; init; }
+        [Required]
+        public ClientType Type { get; init; }
+        [Required]
+        public Document Document { get; init; }
+    }
+}
