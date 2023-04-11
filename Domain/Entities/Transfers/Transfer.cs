b90be015e5b0c4b3df3f3c2b8cbdcfeadf77bf8e
Feitoza <thiagobritofeitoza@gmail.com>
2023-04-11 15:19:40 -0300

    feat: added static factory Transfer

--- Domain/Entities/Transfers/Transfer.cs
+++ Domain/Entities/Transfers/Transfer.cs
@@ -3,8 +3,20 @@ namespace PicPayLite.Domain.Tranfers
     public class Transfer
     {
         public Guid Id { get; private set; }
-        public int Amount{ get; private set; }
-        public Sender Sender { get; set; }
-        public Recipient Recipient { get; set; }
+        public float Amount{ get; private set; }
+        public Sender Sender { get; private set; }
+        public Recipient Recipient { get; private set; }
+
+        private Transfer(float amount, Sender sender, Recipient recipient)
+        {
+            Amount = amount;
+            Sender = sender;
+            Recipient = recipient;
+        }
+
+        public static Transfer Create(float amount, Sender sender, Recipient recipient)
+        {
+            return new Transfer(amount, sender, recipient);
+        }
     }
 }
