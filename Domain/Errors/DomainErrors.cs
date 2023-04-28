9328236b8031c0b9de2452d030fcf9afc1ff7179
Feitoza <thiagobritofeitoza@gmail.com>
2023-04-28 09:47:53 -0300

    feat: added new error

--- Domain/Errors/DomainErrors.cs
+++ Domain/Errors/DomainErrors.cs
@@ -7,6 +7,7 @@ namespace PicPayLite.Domain.Errors
         public static class Accounts
         {
             public static readonly Error AccountNotFound = new("AccountNotFound");
+            public static readonly Error AccountAlreadyExist = new("AccountAlreadyExist");
             public static readonly Error InsuficientBalance = new("InsuficientBalance");
             public static readonly Error InvalidAmountValue = new("InvalidAmountValue");
 
