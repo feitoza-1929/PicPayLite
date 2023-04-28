4449d484a50ead64d0864164e87adfafadf42fcd
Feitoza <thiagobritofeitoza@gmail.com>
2023-04-28 09:49:58 -0300

    feat: implemented get client handle

---
+++ Application/Handlers/Account/AccountGetBalanceHandleAsync.cs
@@ -0,0 +1,36 @@
+using FluentResults;
+using PicPayLite.Application.Handlers.Interfaces;
+using PicPayLite.Domain.Accounts;
+using PicPayLite.Domain.Clients;
+using PicPayLite.Domain.Repositories;
+using PicPayLite.Domain.ValueObjects;
+using PicPayLite.Infrastructure;
+
+namespace PicPayLite.Application.Handlers
+{
+
+    public class AccountGetBalanceHandleAsync : IAccountGetBalanceHandleAsync
+    {
+        private readonly object _dbContext;
+        private readonly IAccountRepository _accountRepository;
+        private readonly IClientRepository _clientRepository;
+
+        public AccountGetBalanceHandleAsync(
+            IAccountRepository accountRepository,
+            IClientRepository clientRepository,
+            ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _accountRepository = accountRepository;
+            _clientRepository = clientRepository;
+        }
+
+        public async Task<Result<Balance>> GetAsync(int accountNumber)
+        {
+            Account account = 
+                await _accountRepository.GetAccountByNumber(accountNumber);
+
+            return Result.Ok(account.Balance);
+        }
+    }
+}
