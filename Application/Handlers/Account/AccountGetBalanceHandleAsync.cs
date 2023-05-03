da1f17debac2abc1020b155016f99afbdbb97298
Feitoza <thiagobritofeitoza@gmail.com>
2023-05-03 20:15:32 -0300

    feat: account validation

--- Application/Handlers/Account/AccountGetBalanceHandleAsync.cs
+++ Application/Handlers/Account/AccountGetBalanceHandleAsync.cs
@@ -2,6 +2,7 @@ using FluentResults;
 using PicPayLite.Application.Handlers.Interfaces;
 using PicPayLite.Domain.Accounts;
 using PicPayLite.Domain.Clients;
+using PicPayLite.Domain.Errors;
 using PicPayLite.Domain.Repositories;
 using PicPayLite.Domain.ValueObjects;
 using PicPayLite.Infrastructure;
@@ -27,8 +28,14 @@ namespace PicPayLite.Application.Handlers
 
         public async Task<Result<Balance>> GetAsync(int accountNumber)
         {
+            if(accountNumber < 1000 || accountNumber > 9999)
+                return Result.Fail(DomainErrors.Account.InvalidAccountNumber);
+                
             Account account = 
                 await _accountRepository.GetAccountByNumber(accountNumber);
+            
+            if(account is null)
+                return Result.Fail(DomainErrors.Account.AccountNotFound);
 
             return Result.Ok(account.Balance);
         }
